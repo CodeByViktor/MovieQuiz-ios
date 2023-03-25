344b56f4a534fdc3890693da7c832c52295c41ba
CodeByViktor <cool-kyl@yandex.ru>
2023-03-25 09:51:05 +0300

    Added notify (vibrate), reduce code repeating, set status bar to white color

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -8,7 +8,7 @@ final class MovieQuizViewController: UIViewController {
       // совпадает с названием картинки афиши фильма в Assets
       let image: String
       // строка с вопросом о рейтинге фильма
-      let text: String
+      let text: String = "Рейтинг этого фильма больше чем 6?"
       // булевое значение (true, false), правильный ответ на вопрос
       let correctAnswer: Bool
     }
@@ -36,6 +36,7 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet weak private var textLabel: UILabel!
     @IBOutlet weak private var counterLabel: UILabel!
     
+    let notificationGenerator = UINotificationFeedbackGenerator()
     // переменная с индексом текущего вопроса, начальное значение 0
     // (по этому индексу будем искать вопрос в массиве, где индекс первого элемента 0, а не 1)
     private var currentQuestionIndex = 0
@@ -46,52 +47,42 @@ final class MovieQuizViewController: UIViewController {
     private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true
         ),
         QuizQuestion(
             image: "The Dark Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true
         ),
         QuizQuestion(
             image: "Kill Bill",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true
         ),
         QuizQuestion(
             image: "The Avengers",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true
         ),
         QuizQuestion(
             image: "Deadpool",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true
         ),
         QuizQuestion(
             image: "The Green Knight",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: true
         ),
         QuizQuestion(
             image: "Old",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false
         ),
         QuizQuestion(
             image: "The Ice Age Adventures of Buck Wild",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false
         ),
         QuizQuestion(
             image: "Tesla",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false
         ),
         QuizQuestion(
             image: "Vivarium",
-            text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false
         ),
     ]
@@ -149,6 +140,9 @@ final class MovieQuizViewController: UIViewController {
         
         if isCorrect {
             correctAnswers += 1
+            notificationGenerator.notificationOccurred(.success)
+        } else {
+            notificationGenerator.notificationOccurred(.error)
         }
         
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
